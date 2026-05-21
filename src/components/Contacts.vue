3d7c5afcf9b07233d13ca541c158b96905b66f85
Gerald Balantac Valencia <gerald.valencia@deped.gov.ph>
2026-05-21 14:09:16 +0800

    initial commit

---
+++ src/components/Contacts.vue
@@ -0,0 +1,36 @@
+<template>
+	
+	<h1 class="text-center my-4 pt-5" id="contact">Contact</h1>
+            <div class="contact-section">
+                <div class="row align-items-center mt-4">
+                    <div class="col-md-6 map-container">
+                        <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=centro%20escolar%20university%20manila&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                    <div class="col-md-6">
+                        <form>
+                            <div class="mb-3">
+                                <input type="text" class="form-control contact-form-control" placeholder="First Name M.I. Last Name">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control contact-form-control" placeholder="Email">
+                            </div>
+                            <div class="mb-3">
+                                <textarea class="form-control contact-form-control" rows="6" placeholder="Message"></textarea>
+                            </div>
+                            <div class="form-footer">
+                                <div class="social-icons">
+<!--                                <a href="https://www.facebook.com/profile.php?id=100085701498879" id="facebook"><i class="fab fa-facebook"></i></a> -->
+                                    <a href="https://www.linkedin.com/in/charles-babbage-8291a6211/" id="linkedin"><i class="fab fa-linkedin"></i></a>
+                                    <a href="https://gitlab.com/cbabbage0991" id="gitlab"><i class="fab fa-gitlab"></i></a>
+                                    <a href="https://github.com/cbabbage0991" id="github"><i class="fab fa-github"></i></a>
+                                </div>
+                                <button type="submit" class="submit-btn pl-5 pr-5">Submit</button>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+ 
+
+</template>
